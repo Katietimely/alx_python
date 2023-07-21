6f215911da51d64b51b0bb891591858eca625f82
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-21 11:41:13 +0100

    my fourth commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -12,4 +12,6 @@ else:
 if number > 5:
     print("and is greater than 5")
 elif number == 0:
-    print(" and is less than 6 and not 0")
+    print("and is 0")
+else:
+    print("and is less than 6 and 0")
--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,2 +1,2 @@
-for i in range(99):
-    print("{:d}  = 0x{:0x}".format(i, i))
+for number in range(99):
+    print("{:d} = 0x{:0x}".format(number, number))
