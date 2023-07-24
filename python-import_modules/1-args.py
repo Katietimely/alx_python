4e650bd36ebca3ca39ce5de6eafe7b302e04936d
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-24 03:07:16 +0100

    my python commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,2 +1,21 @@
 #!/usr/bin/pyhon3
 
+
+import sys
+
+if __name__ == "__main__":
+     num_args = len(sys.argv) - 1
+
+     if num_args == 0:
+          print("0 arguements.")
+     
+     elif num_args == 1:
+          print("1 argument:")
+
+     else:
+          print("{} arguements:".format(num_args))
+              
+
+     if num_args > 0:
+        for num in range(1, len(sys.argv)):
+           print("{}:{[num]}".format(num, sys.argv))
---
+++ python-import_modules/args_program.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+sys.argv = ["Hello Holberton School 98 Battery street"]
+argv[0] = "args_program.py"
+argv[1] = "Hello"
+argv[2] = "Holberton"
+argv[3] = "School"
+argv[4] = "98"
+argv[5] = "Battery"
+argv[6] = "street"
