900e7839549dd4c2ce2fcda1fffaf50df09b3ca4
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-25 05:01:50 +0100

    my first commit

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    char_list = list(my_string)
+    while'c' in char_list:
+        char_list.remove('c')
+    while 'C' in char_list:
+        char_list.remove('C')
+    return ''.join(char_list)
+
+print(no_c("Holberton School"))
+print(no_c("Chicago"))
+print((no_c("C is fun")))
---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
+
+    rows = [" ".join(map("{}".format, row))for row in matrix]
+    print("\n".join(rows))
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
+
