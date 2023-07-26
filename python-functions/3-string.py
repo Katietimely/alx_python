e2d2e45ead85fe71c3d55430fe2c90f5eb6bf78c
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-26 20:19:16 +0100

    my python commit

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -3,7 +3,8 @@ def reverse_string(string):
     return string[::-1]
 
 
-print(reverse_string("Hello"))
-print(reverse_string(""))
-print(reverse_string("madam"))
-print(reverse_string("Hello,world!"))
+if __name__ == "__main__":
+    print(reverse_string("Hello"))
+    print(reverse_string(""))
+    print(reverse_string("madam"))
+    print(reverse_string("Hello,world!"))
---
+++ python-functions/README.md
@@ -0,0 +1,4 @@
+# project description
+This project is based on the programmming language: python
+there is no function without a definition
+
