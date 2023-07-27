ed746b8b981206856b66171844a188c8bab2ca3c
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-27 21:17:20 +0100

    my python commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-
-
-    rows = [(("{:d}".format, row))for row in matrix]
-    print("\n".join(rows))
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]))
+            else:
+                print("{:d}".format(row[i]), end=" ")
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
