c3d1c05e20d51bd96ebadbe7d28c2d7ee3fd3231
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-27 20:06:15 +0100

    my python commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-
-
 import sys
 
 if __name__ == "__main__":
@@ -8,11 +5,11 @@ if __name__ == "__main__":
   num_args = len(args)
 
   if num_args == 0:
-    print("0 arguements.")
+    print("0 arguments.")
   else:
-    print(f"{num_args} arguement{'s' if num_args > 1 else ''}:")
+    print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
     for i, arg in enumerate(args, 1):
-      print(f"{i}: {arg}")
+       print(f"{i}: {arg}")
 
               
      
