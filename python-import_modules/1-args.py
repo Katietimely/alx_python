cec142cba97759212b58862eb9698260af0705e6
Irabor Kate Chika <iraborkate29@gmail.com>
2023-07-27 17:55:57 +0100

    my fifth commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 
-from main import argv
+import sys
 
 if __name__ == "__main__":
-  argv_list = argv[1:]
-  num_args = len(argv_list)
+  args = sys.argv[1:]
+  num_args = len(args)
 
   if num_args == 0:
     print("0 arguements.")
   else:
     print(f"{num_args} arguement{'s' if num_args > 1 else ''}:")
-    for i,arg in enumerate(argv_list,1):
+    for i,arg in enumerate(args,1):
       print(f"{i}: {arg}")
 
               
--- python-import_modules/main.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/pyhon3
-
-def add(a, b):
-    return a + b
-
-argv = ["args_program.py", "Hello", "Holberton", "School", "98", "Battery", "street"]
