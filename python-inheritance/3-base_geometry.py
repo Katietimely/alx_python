e560203a0f67fca3f213654d92dcc4a74afeb57e
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-03 19:07:09 +0100

    my own commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,9 @@
 #1/usr/bin/python3
+"""module of base geometry"""
 class BaseGeometry:
+    """class defined of baseGeometry
+      with no methods in it
+    """
     pass
 bg = BaseGeometry()
 
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
