e343cb3351044f0b052a94b76f5f5c087aa829d1
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-03 15:40:04 +0100

    my forex commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,14 +4,36 @@ module of a square class with private and public attributes
 """
 class Square:
      def __init__(self, size=0):
-        self.__size = size
         """ this is a method(function) with just one arguement"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-          self.__size = size
+        
+        self.__size = size
+
+     def area(self):
+         """calculate the area """
+         return self.__size ** 2
+      
+     def perimeter(self):
+         """claculate the perimeter of the square"""
+         return 4 * self.__size
+     
+     def get_size(self):
+         """merhod square"""
+         return self.__size
+     
+     def set_size(self, new_size):
+         """method square
+         args:new_size(int)
+         """
+         if not isinstance(new_size, int):
+             raise TypeError("size must be an integer")
+         if new_size < 0:
+             raise ValueError("size must be >= 0")
+         self.__size = new_size
+         
 
 my_square_1 = Square(3)
 """a basic method to a method describing the attribute my_square and raising an exception"""
