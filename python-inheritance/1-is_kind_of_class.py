fc9075183ff38b11700107dfe23e925b83f28847
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-03 19:01:53 +0100

    my own commit

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
+"""module of distinguishing same class
+   
+"""
 def is_kind_of_class(obj, a_class):
+    """method of is_same_class
+      obj: this method has an object of obj
+      a_class: this method has an object of a_class
+    """
     return isinstance(obj, a_class)
 
-a = 1
-if is_kind_of_class(a, int):
-    print("{} comes from {}".format(a, int.__name__))
-if is_kind_of_class(a, float):
-    print("{} comes from {}".format(a, float.__name__))
-if is_kind_of_class(a, object):
-    print("{} comes from {}".format(a, object.__name__))
