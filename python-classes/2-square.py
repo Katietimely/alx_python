b4b85ef685426df5424adbe2eded80c53ad46791
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-03 15:47:02 +0100

    my own commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -16,6 +16,24 @@ class Square:
      def area(self):
          """A method defining the area of a given space """
          return self.__size ** 2
+     def perimeter(self):
+         """claculate the perimeter of the square"""
+         return 4 * self.__size
+     
+     def get_size(self):
+         """merhod square"""
+         return self.__size
+     
+     def set_size(self, new_size):
+         """method square
+         args:new_size(int)
+         """
+         if not isinstance(new_size, int):
+             raise TypeError("size must be an integer")
+         if new_size < 0:
+             raise ValueError("size must be >= 0")
+         self.__size = new_size
+         
 
 my_square_1 = Square(3)
 """a basic method decribing the attribute my_square and raising an exception"""
