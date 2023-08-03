de9f3d5c8093ac6f44e29b6e5b234af343f63231
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-03 15:57:43 +0100

    my python commit

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -35,37 +35,5 @@ class Square:
          self.__size = new_size
          
 
-my_square_1 = Square(3)
-"""a basic method to a method describing the attribute my_square and raising an exception"""
-print(type(my_square_1))
-print(my_square_1.__dict__)
 
-
-my_square_2 = Square()
-"""a basic method decribing the attribute my_square and raising an exception"""
-print(type(my_square_2))
-print(my_square_2.__dict__)
-
-try:
-  print(my_square_1.size)
-except Exception as e:
-        print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_3 = Square("3")
-    print(type(my_square_3))
-    print(my_square_3.__dict__)
-except Exception as e:
-    print(e)
-
-try:
-    my_square_4 = Square(-89)
-    print(type(my_square_4))
-    print(my_square_4.__dict__)
-except Exception as e:
-     print(e)
+    
