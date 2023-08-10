b78dfb3063ea82ff865dab6ab17ba769e601ff40
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-10 07:10:17 +0100

    my second com

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -166,17 +166,12 @@ class Rectangle(Base):
 
          Args:
           *args: a list of arguement
-         """    
-         if len(args) > 0:
-          self.id = args[0]
-         if len(args) > 1:
-          self.id = args[1]
-         if len(args) > 2:
-          self.id = args[2]
-         if len(args) > 3:
-          self.id = args[3]
-         if len(args) > 4:
-          self.id = args[4]
+         """   
+         if args:
+             attribute = ['id', 'width', 'height', 'x', 'y'] 
+         for i, arg in enumerate(args):
+             if i < len(attribute):
+                 setattr(self, attribute[i], arg)
         
          
 
