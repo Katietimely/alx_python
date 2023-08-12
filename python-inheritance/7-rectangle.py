66e6aa5e9eacaeb8e88c07eec8e118ead681dee2
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-12 02:41:38 +0100

    alx commit

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,43 +1,6 @@
 #!/usr/bin/python3
 """RECTANGLE CLASS"""
-class BaseGeometry:
-    """basegeometry class"""
-class BaseGeometryMetaClass(type):
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
-class BaseGeometry(metaclass=BaseGeometryMetaClass):
-    """
-    A class inheriting the class BaseGeometry
-    """
-    def __dir__(self): 
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    pass 
-
-    def area(self):
-        """method area to get result
-        Args:
-          name(str): the name of the value
-          value: the value has to be validated
-         Raises:
-          TypeError: if the value is not an integer
-          ValueError: if the value 
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """method integer_validator to determine whether an integer or not
-        Args:
-          name(str): the name of the value
-          value: the value has to be validated
-         Raises:
-          TypeError: if the value is not an integer
-          ValueError: if the value i
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
         
 class Rectangle(BaseGeometry):
     """rectangle inheritance"""
