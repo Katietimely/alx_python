9293d54d6d2531169c6b24338019b0d278503d32
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-12 02:28:12 +0100

    my fist

--- python-more_data_structures/1-main.py
+++ python-more_data_structures/1-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-common_elements = __import__('1-common_elements').common_elements
+common_elements = __import__('1-common_elements').common_element
 
 set_1 = { "Python", "C", "Javascript" }
 set_2 = { "Bash", "C", "Ruby", "Perl" } 
