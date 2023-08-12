65c357a36b224e5df7113119362bbee2c1ac0d7b
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-12 17:27:17 +0100

    alx commit

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,5 +1,4 @@
-
-def common_element(set_1, set_2):
+def common_elements(set_1, set_2):
     common_set = set()
 
     for element in set_1:
@@ -8,8 +7,8 @@ def common_element(set_1, set_2):
 
     return common_set
 
+
 set1 = {1, 2, 3, 4, 5}
 set2 = {4, 5, 6, 7, 8}
 
-common_element_set = common_element(set1, set2)
-   
+common_elements_set = common_elements(set1, set2)
