27694ea34a3735250068fdd9c6e23dafce0e7596
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-12 17:02:03 +0100

    commit mode

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+#!/usr/bin/python3
+
 for i in range(10):
-    for j in range(i+1, 10):
-        if i !=8 or j != 9:
-            print("{}{:01d}, ".format(i,j), end="")
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{:02d}".format(i * 10 + j))
         else:
-            print("{}{:01d}, ".format(i, j), end="\n")
+            print("{:02d}, ".format(i * 10 + j), end="")
 
 
       
