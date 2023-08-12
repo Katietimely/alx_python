40592b8826169fa5cd2f549e36772fbd25268fc6
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-12 02:13:41 +0100

    my fisrst commit

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 
 def fibonacci_sequence(n):
-    if n == 0:
-        fb=[]
+    if n <= 0:
+        return []
+    
+    fb =[0]
+    if n == 1:
         return fb
-    elif n == 1:
-        fb=[1]
-        return fb
-    else:
-        fb = [0, 1]
+    
+    fb.append(1)
     for i in range(2, n):
-              fb.append(fb[i-1 + fb[i-2]])
+              next_fb = fb[i - 1] + fb[i - 2]
+              fb.append(next_fb)
+
     return fb
 
 
