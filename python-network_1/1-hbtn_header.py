10f571150429f2d4a3add800311b90e9b52949d1
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-13 23:32:57 +0100

    requests heaader

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,24 +2,14 @@ import requests
 
 import sys
 
-def get_request_id(url):
-    try:
-        response = requests.get(url)
-        request_id = response.headers.get('X-Requests-Id')
-
-        if request_id:
-            return request_id
-        else:
-            return "X-Request-Id not found in response headers"
-    except requests.RequestException as e:
-        return f"Error sending request: {e}"
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <url>")
-        sys.exit(1)
+if len(sys.argv) != 2:
+    print("Usage: {} <URL>".format(sys.argv[0]))
+    sys.exit(1)
     
-    url = sys.argv[1]
-    request.id = get_request_id(url)
-    print(request_id)
+url = sys.argv[1]
+
+response = requests.get(url)
+request_id = response.header.get('X-Request-Id')
+
+
+print(request_id)
