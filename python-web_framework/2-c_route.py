b2127f90280fe5a591b6aff9c88cbad0ba936673
Irabor Kate Chika <iraborkate29@gmail.com>
2023-08-28 17:12:42 +0100

    flasking

---
+++ python-web_framework/2-c_route.py
@@ -0,0 +1,28 @@
+"""
+this module is called flask and imported from flask
+"""
+from flask import Flask
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+#route created at the localhost
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+# a defined function of hello_hbnb to return a string
+    return "Hello HBNB!"
+
+# route created to dislocate the previous setting
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+#route created to the previous route setting
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    decoded_text = unquote(text).replace('_', ' ')
+    return "C {}".format(decoded_texts)
+
+if __name__ == "__main__":
+# activating the debug: ON
+    app.run(debug=True)
