51f23dee7f498f2291beab8f12b05a90873ff7ce
Irabor Kate Chika <iraborkate29@gmail.com>
2023-09-04 19:19:54 +0100

    web html

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     cursor = database.cursor()
 
     cursor.execute(
-        "SELECT cities.id, cities.name, states.name \
-         FROM cities JOIN states ON states_id = states.id \
+        "SELECT cities.id, cities.name, states.name FROM cities \
+         JOIN states ON cities.states_id = states.id \
          ORDER BY cities.id ASC")
 
     cities = cursor.fetchall()
